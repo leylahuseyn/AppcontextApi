8ada978767b5fe7f8054c0b413227f64a9703499
leylahuseyn <79pg4t1@code.edu.az>
2024-08-01 14:14:05 +0400

    commit

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -3,21 +3,57 @@ import { Link } from 'react-router-dom'
 import Maincontext from '../Context/Context'
 
 const Header = () => {
-    const {fav}=useContext(Maincontext)
-  return (
-    <div>
-        <Link to="/">Home</Link>
-        <Link to="/WishList">WishList</Link>
-        {
-            fav.length>0 &&
-            (
-                <span>({fav.length})</span>
-            )
-        }
-        <Link to="/Product">Product</Link>
-        <Link to="/Contact">Contact</Link>
-    </div>
-  )
+    const { fav } = useContext(Maincontext)
+    return (
+        // <div>
+        //     <Link to="/">Home</Link>
+        //     <Link to="/WishList">WishList</Link>
+        // {
+        //     fav.length>0 &&
+        //     (
+        //         <span>({fav.length})</span>
+        //     )
+        // }
+        //     <Link to="/Product">Product</Link>
+        //     <Link to="/Contact">Contact</Link>
+        // </div>
+        <header>
+            <nav className="navbar navbar-expand-lg  ">
+                <div className="container">
+
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <Link className="navbar-brand navbar-nav me-auto mb-2 mb-lg-0" to="/" >Home</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <span className="navbar-text">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" to="/Product">Products
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/WishList">WishList</Link>
+                                    {
+                                        fav.length > 0 &&
+                                        (
+                                            <span>({fav.length})</span>
+                                        )
+                                    }
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/Contact">Contact</Link>
+                                </li>
+                            </ul>
+                        </span>
+
+                    </div>
+                </div>
+            </nav>
+            <hr />
+
+        </header>
+    )
 }
 
 export default Header
--- src/companents/Card.tsx
+++ src/companents/Card.tsx
@@ -1,18 +1,22 @@
 import React, { useContext } from 'react'
 import "./card.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import Maincontext from '../Context/Context'
 import { Link } from 'react-router-dom'
 const Card = ({item}) => {
     const {addTOFAv}=useContext(Maincontext)
   return (
-    <div className='col-lg-3 col-md-6 mr-3 mb-2'>
+    <div className='col-lg-3 col-md-6 mr-3 mb-2 fs-5'>
      <div className="card" style={{width: "18rem;"}}>
+      <FontAwesomeIcon className='hearticon'  onClick={()=>{
+       
+        addTOFAv(item)
+    }} icon={faHeart} />
  <Link to={`/${item.id}`}> <img className="card-img-top" src={item.image} alt="Card image cap"/></Link>
   <div className="card-body">
-    <h5 className="card-title">{item.title}</h5>
-    <button onClick={()=>{
-        addTOFAv(item)
-    }}>add to fav</button>
+    <h5 className="card-title fs-6   ">{item.title}</h5>
+    
   </div>
 </div>
     </div>
